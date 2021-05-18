462f91e4b02336172295d72cfefbce0d7700a571
Nikita Trafilkin <ntrafilkin88@gmail.com>
2021-05-18 11:40:42 +0300

    adds static

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
 const { PORT } = require("./config/config");
@@ -6,6 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(express.json({ extended: true, limit: "30mb" }));
+app.use(express.static("public"));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("tiny"));
 }
