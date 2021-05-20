14db916994a03bae8dc54b69783b281d44fa4ee4
Nikita Trafilkin <ntrafilkin88@gmail.com>
2021-05-20 19:12:48 +0300

    more ayth thunks fixes

--- client/src/slices/auth.slice.js
+++ client/src/slices/auth.slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toaster } from "evergreen-ui";
 import Api from "../utils/Api";
 
 const authSlice = createSlice({
@@ -17,29 +18,35 @@ const { reducer, actions } = authSlice;
 const { authSuccess, authFail } = actions;
 
 export const authUser = () => async (dispatch) => {
-  const data = await Api.authUser();
-  if (!data.errors) {
-    dispatch(authSuccess(data));
+  const { data, errors, message } = await Api.authUser();
+  if (!errors) {
+    dispatch(authSuccess(data.data));
+    message && toaster.success(message, { duration: 3 });
   } else {
     dispatch(authFail());
+    message && toaster.danger(message, { duration: 3 });
   }
 };
 
 export const registerUser = (user) => async (dispatch) => {
-  const data = await Api.registerUser(user);
-  if (!data.errors) {
-    dispatch(authSuccess(data));
+  const { data, errors, message } = await Api.registerUser(user);
+  if (!errors) {
+    dispatch(authSuccess(data.data));
+    message && toaster.success(message, { duration: 3 });
   } else {
     dispatch(authFail());
+    message && toaster.danger(message, { duration: 3 });
   }
 };
 
 export const loginUser = (credentials) => async (dispatch) => {
-  const data = await Api.loginUser(credentials);
-  if (!data.errors) {
-    dispatch(authSuccess(data));
+  const { data, errors, message } = await Api.registerUser(user);
+  if (!errors) {
+    dispatch(authSuccess(data.data));
+    message && toaster.success(message, { duration: 3 });
   } else {
     dispatch(authFail());
+    message && toaster.danger(message, { duration: 3 });
   }
 };
 
