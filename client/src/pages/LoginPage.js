52c01c5ddc60e7b2de190e7e151f5f23b7bde306
Nikita Trafilkin <ntrafilkin88@gmail.com>
2021-05-21 09:38:47 +0300

    adds loginpage

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Pane, Heading, Paragraph, Link } from "evergreen-ui";
+import LoginForm from "../components/auth/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <Pane
+      width="100%"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Pane
+        border="default"
+        borderRadius="4px"
+        padding="30px"
+        display="flex"
+        justifyContent="center"
+        alignItems="stretch"
+        flexDirection="column"
+        width="500px"
+        backgroundColor="#fff"
+      >
+        <Heading textAlign="center" size={700}>
+          Signin
+        </Heading>
+        <LoginForm />
+        <Paragraph size={500} marginTop="10px">
+          Don't have an account?{" "}
+          <Link size={500} cursor="pointer" href="#">
+            Signup
+          </Link>
+        </Paragraph>
+      </Pane>
+    </Pane>
+  );
+};
+
+export default LoginPage;
