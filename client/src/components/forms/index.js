0c1d4391f062b21faecc0bff676012af0c4d39ec
Nikita Trafilkin <ntrafilkin88@gmail.com>
2021-05-21 14:54:11 +0300

    formtextarea export

--- client/src/components/forms/index.js
+++ client/src/components/forms/index.js
@@ -1,5 +1,6 @@
 export { default as Form } from "./Form";
 export { default as FormField } from "./FormField";
+export { default as FormTextArea } from "./FormTextArea";
 export { default as FormFilePicker } from "./FormFilePicker";
 export { default as FormTagInput } from "./FormTagInput";
 export { default as FormCheckbox } from "./FormCheckBox";
